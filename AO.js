f8899c270ac31a5fa7be5107755db4fddf4e6b1e
Bojama <86303898+BojamaV@users.noreply.github.com>
2022-05-03 15:05:03 -0400

    Create AO.js

---
+++ AO.js
@@ -0,0 +1,81 @@
+var a = {value: 1};
+var b = a
+    var c = b
+        var d = c
+            var e = d
+var f = e
+var g = f
+var h = g
+var i = h
+var j = i
+var k = j
+var l = k
+var m = l
+var n = m
+var o = n
+var p = o
+var q = p
+var r = q
+var s = r
+var t = s
+var u = t
+var v = u
+var w = v
+var x = w 
+var y = x
+var z = y
+    
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+console.log(n);
+console.log(o);
+console.log(p);
+console.log(q);
+console.log(r);
+console.log(s);
+console.log(t);
+console.log(u);
+console.log(v);
+console.log(w);
+console.log(x);
+console.log(y);
+console.log(z);
+
+console.log(a == b);
+console.log(a == c);
+console.log(a == d);
+console.log(a == e);
+console.log(a == f);
+console.log(a == g);
+console.log(a == h);
+console.log(a == i);
+console.log(a == j);
+console.log(a == k);
+console.log(a == l);
+console.log(a == m);
+console.log(a == n);
+console.log(a == o);
+console.log(a == p);
+console.log(a == q);
+console.log(a == r);
+console.log(a == s);
+console.log(a == t);
+console.log(a == u);
+console.log(a == v);
+console.log(a == w);
+console.log(a == x);
+console.log(a == y);
+console.log(a == z);
+
+console.log(document.readyState);
